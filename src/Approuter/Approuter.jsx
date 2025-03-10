2f4f345505f76359408c579f96754f8ae1dc580e
Rolf M <rolfie@outlook.dk>
2025-03-10 10:59:00 +0100

    Approuter
    Opsætning Approuter

---
+++ src/Approuter/Approuter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes } from 'react-router-dom'
+
+function Approuter() {
+  return (
+    <Routes>
+        <Route index element={}></Route>
+    </Routes>
+  )
+}
+
+export default Approuter
