c7a4acf554683aae10f5f2cef711fce51b0d30fb
Rolf M <rolfie@outlook.dk>
2025-03-10 11:37:16 +0100

    Oprette forside fil
    Oprettelse forside, scss moduler osv

---
+++ src/pages/Frontpage/Frontpage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Frontpage.module.scss'
+
+function Frontpage() {
+  return (
+    <div className = {style.mainArea}></div>
+  )
+}
+
+export default Frontpage
