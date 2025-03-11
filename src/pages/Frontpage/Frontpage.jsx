aa9d79e41e840fec1a2f1a503b8e8c74f7084e2a
Rolf M <rolfie@outlook.dk>
2025-03-11 13:04:11 +0100

    Styling heroBillede
    Gav heroBiilede en størrelse samt en norepeat

--- src/pages/Frontpage/Frontpage.jsx
+++ src/pages/Frontpage/Frontpage.jsx
@@ -14,7 +14,7 @@ function Frontpage() {
 
         <hr></hr>
 {/* Template string bruges til at hente det importeret billede */}
-      <article style={{backgroundImage: `url(${heroBillede})`}}> 
+      <article className={style.heroArticle} style={{backgroundImage: `url(${heroBillede})`}}> 
           <h1> Den Grønne Avis</h1>
           <p>Vi går forest i kampen om klimaet ved at give 2 kr. til
 klima-venlige formål, hver gang du handler brugt på Den
