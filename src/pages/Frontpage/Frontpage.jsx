04cac71e5f761c4f20fb7e2c56d0f477036b0059
Rolf M <rolfie@outlook.dk>
2025-03-11 12:53:12 +0100

    Forside styling plus import billeder
    Import heroBillede plus styling forside elementer

--- src/pages/Frontpage/Frontpage.jsx
+++ src/pages/Frontpage/Frontpage.jsx
@@ -1,37 +1,38 @@
 import React from 'react'
 import style from './Frontpage.module.scss'
+import heroBillede from "../../assets/herobillede.jpg"
 
 function Frontpage() {
   return (
-    <div className = {style.mainArea}>
+    <section className = {style.mainArea}>
 
         <hr></hr>
         <h2>Udvalgte produkter</h2>
-        <div>
+        <article>
             placeholder
-        </div>
+        </article>
 
         <hr></hr>
-
-        <div> 
+{/* Template string bruges til at hente det importeret billede */}
+      <article style={{backgroundImage: `url(${heroBillede})`}}> 
           <h1> Den Grønne Avis</h1>
           <p>Vi går forest i kampen om klimaet ved at give 2 kr. til
 klima-venlige formål, hver gang du handler brugt på Den
 Grønne Avis
 </p>
-        </div>
+        </article>
         <hr />
 
 
         <h2>
           Populære Kategorier
         </h2>
-        <div>
+        <article>
 
           placeholder
-        </div>
+        </article>
 
-        <div>
+        <article>
           <div>
             <h3>Donationer til Dato</h3>
             <p>Sammen med dig har vi siden starten indsamlet:</p>
@@ -52,8 +53,8 @@ Grønne Avis
             <p>Tak fordi du handler brugt, med omtanke for klimaet</p>
 
           </div>
-        </div>
-    </div>
+        </article>
+    </section>
   )
 }
 
