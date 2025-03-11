4765840faf9f847cc98573dbcbd59be6cedc86c2
Rolf M <rolfie@outlook.dk>
2025-03-11 17:10:49 +0100

    Kategoriside opsætning
    Opsætning af kategorierne på ny side. Samt oprettelse af kategorisiden i Approuter.

---
+++ src/components/CardsKategoriPage/CardsKategoriPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./CardsKategoriPage.module.scss"
+
+
+
+function CardsKategoriPage() {
+  return (
+    <div className={style.cardsDiv}><p>placeholder text</p></div>
+  )
+}
+
+export default CardsKategoriPage
---
+++ src/pages/Kategoripage/Kategoripage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+    
+
+function Kategoripage() {
+  return (
+    <section>
+<hr />
+<h2>Alle Kategorier</h2>
+<ul>
+<li value="">Camping</li>
+            <li value="">Elektronik</li>
+            <li value="">Have og byg</li>
+            <li value="">Tøj og mode</li>
+            <li value="">Cykler</li>
+            <li value="">Biler</li>
+            <li value="">Hobby</li>
+            <li value="">Mobil og telefoni</li>
+            <li value="">Musik instrumenter</li>
+            <li value="">Billede og lyd</li>
+            <li value="">Scooter og knallerter</li>
+            <li value="">Bøger og blade</li>
+            <li value="">Diverse</li>
+</ul>
+
+<article>
+
+
+</article>
+
+    </section>
+  )}
+export default Kategoripage
